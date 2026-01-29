534c570f32d2cf4c30ff2cc3d9e9c018161e0237
Mohit-Chowdhary <mohit24c@gmail.com>
2026-01-30 00:44:38 +0530

    m

--- tle31/900/1624B.cpp
+++ tle31/900/1624B.cpp
@@ -14,11 +14,25 @@ int main(){
         cin>>a>>b>>c;
         bool valid = true;
         //cout<<a<<" "<<b<<" "<<c<<"\n";
+        int new_c = (2*(b-a)+a);
+        int new_b = a+ (c-a)/2;
+        int new_a = (2*b-c);
         if( a-b == b-c){
+            valid = true;
+        }
+        else if( new_c>0 && new_c%c ==0 ){
+            valid  = true;
+        }
+        else if(new_a>0 && new_a%a == 0){
+
+        }
+        else if( new_b>0 &&  new_b%b==0 && (c-a)%2==0){
             
         }
+        else{
+            valid = false;
+        }
 
-        
         if(valid) cout<<"YES\n";
         else cout<<"NO\n";
 
